acf961889b465f12fd360c149d4b671256e3a089
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-02 15:30:34 +0200

    feat: load data from csv, add user prompt, check user answer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 # Setup the screen
 screen = turtle.Screen()
@@ -7,6 +8,24 @@ image = "./blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+# Variables
+data = pandas.read_csv("./50_states.csv")
+states = data.state.to_list()
+correct_answers = []
+
+while len(correct_answers) < 50:
+  answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+
+  if answer_state in states:
+    correct_answers.append(answer_state)
+    state = turtle.Turtle()
+    state.hideturtle()
+    state.penup()
+
+    state_data = data[data.state == answer_state]
+    state.goto(int(state_data.x), int(state_data.y))
+    state.write(state_data.state.item())
+
+  
 
 screen.exitonclick()
